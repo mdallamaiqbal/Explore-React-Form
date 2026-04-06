3548b4a9075e4e7397f2c89d962999b2d75ba857
mdallamaiqbal <bd999plus13@gmail.com>
2026-04-06 10:38:27 +0600

    add product table when you can add product and you can see the validation

--- src/Components/ProductManagement/ProductForm.jsx
+++ src/Components/ProductManagement/ProductForm.jsx
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ProductForm = () => {
+const ProductForm = ({handleAddProduct}) => {
+    const [error, setError]=useState('')
     const handleProductSubmit = e =>{
         e.preventDefault();
+        
         const name = e.target.name.value;
         const price = e.target.price.value;
         const quantity = e.target.quantity.value;
-        //console.log(name,price,quantity);
+       //validation
+       if(name.length === 0){
+          setError('Please provide a product name');
+          return
+       }else if(price.length === 0){
+         setError('Please provide a price');
+          return
+       }else if(price < 0){
+        setError('Price can not be negative');
+         return
+       }else if(quantity.length === 0){
+        setError('Please provide a quantity');
+         return
+       }else if(quantity < 0){
+        setError('Quantity can not be negative');
+         return
+       }else{
+        setError('');
+        e.target.reset();
+       }
         const newProduct ={
             name,
             price,
             quantity
         }
-        console.log(newProduct);
-        
+        handleAddProduct(newProduct)
     }
     return (
         <div>
@@ -26,8 +46,8 @@ const ProductForm = () => {
                 <input type="text" name="quantity" placeholder='Quantity' />
                 <br />
                 <input type="submit" value="Submit" />
-
             </form>
+            <p style={{color:'red'}}><small>{error}</small></p>
         </div>
     );
 };
